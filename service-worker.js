7ad930440edf7dc62d9b2fdc342b76abdd536491
Johnssther <j.ahr@hotmail.com>
2020-06-07 17:55:05 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coreui_base/precache-manifest.ee40e2a7b45dac38311308f89fd889ab.js"
+  "/coreui_base/precache-manifest.dc48dac57050e3ce8fbc06fdbf3c7a7f.js"
 );
 
 self.addEventListener('message', (event) => {
