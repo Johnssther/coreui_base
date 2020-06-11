07c940aea1bbae0ad643b99e0391df92dcc50864
Johnssther <j.ahr@hotmail.com>
2020-06-11 13:35:31 -0500

    Updates

---
+++ static/js/40.7053426c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@coreui/coreui-free-react-admin-template"]=this["webpackJsonp@coreui/coreui-free-react-admin-template"]||[]).push([[40],{967:function(e,t,r){"use strict";r.r(t);var a=r(1),c=r.n(a);t.default=function(){return c.a.createElement("h2",null,"reportes")}}}]);
+//# sourceMappingURL=40.7053426c.chunk.js.map
